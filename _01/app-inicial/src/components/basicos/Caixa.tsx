c7735e0f292b83cee55e9b7b9ef5e6b19c58ca5e
VictorMatheusMoreira <vi.matheusm@gmail.com>
2024-09-09 18:05:39 -0300

    nt
    4015

---
+++ _01/app-inicial/src/components/basicos/Caixa.tsx
@@ -0,0 +1,11 @@
+export default function Caixa() {
+    return (
+        <div className={`
+        bg-purple-500  rounded-md p-2 
+         w-64 h-64 text-lg 
+        `}>
+            Caixa
+        </div>
+    )
+
+}
